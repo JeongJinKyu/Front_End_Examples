6618fbf80efd31c0e4a82dd7d0bcc807faaf17fd
JeongJinKyu <jjk3969@naver.com>
2023-07-20 14:59:04 +0900

    [fix] main_page

--- Vue_Practice_Dump/vue_practice/src/components/HelloWorld.vue
+++ Vue_Practice_Dump/vue_practice/src/components/HelloWorld.vue
@@ -1,33 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <section class="hi-i">
+        <h1 class="title">❤Chaining❤</h1>
+        <p>
+            블록체인 기술과 함께하는 기부문화. <br>
+            새로운 기술과 함께 행복한 시간을 만끽해보세요.
+        </p>
+    </section>
 </template>
 
 <script>
@@ -41,18 +19,33 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Single+Day&display=swap');
+
+.hi-i {
+    text-align: center;
+    padding-top: 10vh;
+    background-image: url('../assets/vecteezy_vector-illustration-for-charity-welfare-assistance-concept_6902005.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.title {
+    font-family: 'Dancing Script', cursive;
+    font-size: 7rem;
+    margin-bottom: 2rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+p {
+    font-family: 'Single Day';
+    line-height: 1.7;
+    font-size: 2rem;
+    color : #595959;
 }
-a {
-  color: #42b983;
+section {
+    color : #DB005B;
+    height: 100vh;
+    scroll-snap-align: start;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
 }
 </style>
